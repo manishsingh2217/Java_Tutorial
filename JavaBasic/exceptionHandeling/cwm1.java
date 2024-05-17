a91adfbdf6fa669b4b79f7af0348d532d01a8843
Manish singh <manishupwala731@gmail.com>
2024-05-17 07:16:10 +0530

    This is for testing only

---
+++ JavaBasic/exceptionHandeling/cwm1.java
@@ -0,0 +1,7 @@
+package JavaBasic.exceptionHandeling;
+
+public class cwm1 {
+    public static void main(String[] args) {
+        System.out.println("This is testing java Exception handeling file");
+    }
+}
